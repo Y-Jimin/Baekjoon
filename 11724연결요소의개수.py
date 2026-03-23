4a1c16bb2622fa6a0f7e99bbca71a0e7b19f9cbb
Y-Jimin <jak2899@naver.com>
2026-03-23 15:23:43 +0900

    11724연결요소의개수

---
+++ 11724연결요소의개수.py
@@ -0,0 +1,25 @@
+n,m = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+visited = [False for _ in range(n+1)]
+result = set()
+
+for i in range(m):
+    a,b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+def dfs(a):
+    visited[a] = True
+    result.add(a)
+    for i in graph[a]:
+        if visited[i] == False:
+            dfs(i)
+cnt=0
+while True:
+    if result == set(range(1,n+1)):
+        break
+    for i in range(1,n+1):
+        if i not in result:
+            cnt+=1
+            dfs(i)
+print(cnt) 
